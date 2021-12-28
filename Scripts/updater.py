cc16df873af7346418ee66605ffe25e559921730
CorpNewt <12772521+corpnewt@users.noreply.github.com>
2021-12-28 05:22:40 -0600

    Save reveal and kext debug options in new profile

--- Scripts/updater.py
+++ Scripts/updater.py
@@ -764,7 +764,9 @@ class Updater:
             "Xcode" : self.xcode_opts, 
             "SDK" : self.sdk_over, 
             "DefOnFail" : self.default_on_fail, 
-            "IncrementSDK" : self. increment_sdk
+            "IncrementSDK" : self.increment_sdk,
+            "Reveal" : self.reveal,
+            "Debug" : self.kext_debug
             }
         self.profiles.append(new_pro)
         # Save to file
